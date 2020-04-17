f9d2883d0717cced9ec73e206969c55589d1f9ea
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-18 00:06:54 +0530

    arranged files

---
+++ src/app.js
@@ -0,0 +1,18 @@
+//Setting up express
+const express = require("express");
+const app = express();
+
+//setting up dir path
+const path = require("path");
+const publicDir = path.join(__dirname, "../public");
+
+//using public dir in express
+app.use(express.static(publicDir));
+
+app.get("newYork", (req, res) => {
+  res.sendFile(publicDir + "/new-york.html");
+});
+
+app.listen(3000, () => {
+  console.log("Server is up at port 3000");
+});
